c07fc551f569ea99740fb386777e2e6f6bbcd5e5
Tate Frost <tatefrost96@gmail.com>
2021-12-16 14:41:11 -0700

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const path = require('path')
 
+const Rollbar = require('rollbar')
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'f5c36d564dcb42fb994bc2df9531832a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+  try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+  }
 })
 
 const port = process.env.PORT || 4005
